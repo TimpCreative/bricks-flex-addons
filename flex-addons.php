ecbb001a184ca9d0e5eafe9277f2a924165dcc1b
TimpCreative <hello@timpcreative.com>
2025-05-01 18:53:36 -0600

    Freemius SDK

--- flex-addons.php
+++ flex-addons.php
@@ -6,3 +6,35 @@ Version: 0.1.0
 Author: TimpCreative
 License: GPL-2.0+
 */
+
+if ( ! function_exists( 'bfa_plugin' ) ) {
+    // Create a helper function for easy SDK access.
+    function bfa_plugin() {
+        global $bfa_plugin;
+
+        if ( ! isset( $bfa_plugin ) ) {
+            // Include Freemius SDK.
+            require_once dirname( __FILE__ ) . '/vendor/freemius/start.php';
+            $bfa_plugin = fs_dynamic_init( array(
+                'id'                  => '18913',
+                'slug'                => 'bricks-flex-addons',
+                'type'                => 'plugin',
+                'public_key'          => 'pk_8c55d8f649971139698c631e4ed85',
+                'is_premium'          => false,
+                'has_addons'          => false,
+                'has_paid_plans'      => false,
+                'menu'                => array(
+                    'account'        => false,
+                    'support'        => false,
+                ),
+            ) );
+        }
+
+        return $bfa_plugin;
+    }
+
+    // Init Freemius.
+    bfa_plugin();
+    // Signal that SDK was initiated.
+    do_action( 'bfa_plugin_loaded' );
+}
